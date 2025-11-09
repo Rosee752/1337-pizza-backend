bd32dc0b211eda36ed5fd18737f6b5d315f1ed5f
Rinor Latifi <rinor.latifi@stud.h-da.de>
2025-11-09 15:32:46 +0100

    integration_test

---
+++ tests/integration/api/v1/test_dough.py
@@ -0,0 +1,58 @@
+import pytest
+from decimal import Decimal
+import app.api.v1.endpoints.dough.crud as dough_crud
+from app.api.v1.endpoints.dough.schemas import DoughCreateSchema
+from app.database.connection import SessionLocal
+
+
+@pytest.fixture(scope='module')
+def db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def test_dough_create_read_delete(db):
+    new_dough_name = 'Test Dough'
+    new_dough_price = Decimal('2.5')
+    new_dough_desc = 'Test dough desc'
+    new_dough_stock = 20
+    number_of_doughs_before = len(dough_crud.get_all_doughs(db))
+
+    # Arrange: Instantiate a new user object
+    dough = DoughCreateSchema(
+        name=new_dough_name,
+        price=new_dough_price,
+        description=new_dough_desc,
+        stock=new_dough_stock
+    )
+
+    # Act: Add user to database
+    db_dough = dough_crud.create_dough(dough, db)
+    created_dough_id = db_dough.id
+
+    # Assert: One more user in database
+    doughs = dough_crud.get_all_doughs(db)
+    assert len(doughs) == number_of_doughs_before + 1
+
+    # Act: Re-read user from database
+    read_dough = dough_crud.get_dough_by_id(created_dough_id, db)
+
+    # Assert: Correct user was stored in database
+    assert read_dough.id == created_dough_id
+    assert read_dough.name == new_dough_name
+    assert read_dough.price == new_dough_price
+    assert read_dough.description == new_dough_desc
+    assert read_dough.stock == new_dough_stock
+
+    # Act: Delete user
+    dough_crud.delete_dough_by_id(created_dough_id, db)
+
+    # Assert: Correct number of users in database after deletion
+    doughs = dough_crud.get_all_doughs(db)
+    assert len(doughs) == number_of_doughs_before
+
+    # Assert: Correct user was deleted from database
+    deleted_dough = dough_crud.get_dough_by_id(created_dough_id, db)
+    assert deleted_dough is None
