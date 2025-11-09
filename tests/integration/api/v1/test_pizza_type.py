c416b43efd2de2f98ea7738bf6521c506bcb8c68
stsaraouz <saifeddine.raouzi@stud.h-da.de>
2025-11-09 19:35:53 +0100

    Add and update test_pizza_type.py for Lab4Rose

---
+++ tests/integration/api/v1/test_pizza_type.py
@@ -0,0 +1,55 @@
+import pytest
+from decimal import Decimal
+import app.api.v1.endpoints.pizza_type.crud as pizza_type_crud
+from app.api.v1.endpoints.pizza_type.schemas import PizzaTypeCreateSchema
+from app.database.connection import SessionLocal
+
+@pytest.fixture(scope='module')
+def db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def test_pizza_type_create_read_delete(db):
+    new_pizza_type_name = 'Test Pizza Type'
+    new_pizza_type_price = Decimal('9.99')
+    new_pizza_type_description = 'Delicious test pizza type'
+    number_of_pizza_types_before = len(pizza_type_crud.get_all_pizza_types(db))
+
+    # Arrange: Create a new PizzaType object
+    pizza_type = PizzaTypeCreateSchema(
+        name=new_pizza_type_name,
+        price=new_pizza_type_price,
+        description=new_pizza_type_description
+    )
+
+    # Act: Add pizza_type to database
+    db_pizza_type = pizza_type_crud.create_pizza_type(pizza_type, db)
+    created_pizza_type_id = db_pizza_type.id
+
+    # Assert: One more pizza_type in database
+    pizza_types = pizza_type_crud.get_all_pizza_types(db)
+    assert len(pizza_types) == number_of_pizza_types_before + 1
+
+    # Act: Re-read pizza_type from database
+    read_pizza_type = pizza_type_crud.get_pizza_type_by_id(created_pizza_type_id, db)
+
+    # Assert: Correct pizza_type was stored in database
+    assert read_pizza_type.id == created_pizza_type_id
+    assert read_pizza_type.name == new_pizza_type_name
+    assert read_pizza_type.price == new_pizza_type_price
+    assert read_pizza_type.description == new_pizza_type_description
+
+    # Act: Delete pizza_type
+    pizza_type_crud.delete_pizza_type_by_id(created_pizza_type_id, db)
+
+    # Assert: Correct number of pizza_types in database after deletion
+    pizza_types = pizza_type_crud.get_all_pizza_types(db)
+    assert len(pizza_types) == number_of_pizza_types_before
+
+    # Assert: Correct pizza_type was deleted from database
+    deleted_pizza_type = pizza_type_crud.get_pizza_type_by_id(created_pizza_type_id, db)
+    assert deleted_pizza_type is None
