5b9e27aa7177f215cf1dcde4954f82db560f8da5
Lucas <lucas.roth@stud.h-da.de>
2026-01-13 17:29:58 +0100

    bug fix

--- tests/integration/api/v1/test_stock_ingredients.py
+++ tests/integration/api/v1/test_stock_ingredients.py
@@ -13,6 +13,11 @@ from app.api.v1.endpoints.order.stock_logic.stock_ingredients_crud import (
     reduce_stock_of_ingredients,
     increase_stock_of_ingredients,
 )
+import app.api.v1.endpoints.sauce.crud as sauce_crud
+import decimal
+import app.api.v1.endpoints.sauce.schemas as sauce_schemas
+from app.api.v1.endpoints.sauce.schemas import SpicinessType
+
 
 
 @pytest.fixture(scope='module')
@@ -53,15 +58,40 @@ def sample_topping(db):
     # Cleanup
     topping_crud.delete_topping_by_id(db_topping.id, db)
 
+@pytest.fixture(scope='module')
+def sample_sauce(db):
+    # arrange
+    test_name = 'test_name'
+    test_description = 'test_description'
+    test_stock = 10
+    test_price = decimal.Decimal(3.50)
+    test_spiciness = SpicinessType.HOT
+
+    # act
+    sauce = sauce_schemas.SauceCreateSchema(
+        name=test_name,
+        description=test_description,
+        stock=test_stock,
+        price=test_price,
+        spiciness=test_spiciness,
+    )
+
+    new_sauce = sauce_crud.create_sauce(schema=sauce, db=db)
+    created_sauce_id = new_sauce.id
+    yield new_sauce
+
+    sauce_crud.delete_sauce_by_id(created_sauce_id,db=db)
+
 
 @pytest.fixture
-def sample_pizza_type(db, sample_dough, sample_topping):
+def sample_pizza_type(db, sample_dough, sample_topping, sample_sauce):
     """Create a test pizza type with dough and topping and clean it up after"""
     pizza_type = PizzaTypeCreateSchema(
         name=f'Test Pizza {uuid.uuid4()}',
         price=8.00,
         description='Test pizza',
         dough_id=sample_dough.id,
+        sauce_id=sample_sauce.id,
         toppings=[
             PizzaTypeToppingQuantityCreateSchema(
                 topping_id=sample_topping.id,
